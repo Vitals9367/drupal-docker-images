b092b0e11332f4ae4761ba016ea2e8afdd772164
tuutti <tuutti@iki.fi>
2023-02-07 08:22:41 +0200

    Fixed infinite drush startserver loop when project is not installed

--- local/drupal/entrypoints/30-drush-server.sh
+++ local/drupal/entrypoints/30-drush-server.sh
@@ -2,6 +2,6 @@
 
 if command -v drush &> /dev/null
 then
-  until drush runserver http://127.0.0.1:8888; do echo "program ended with status $?"; done &
+  sleep 5 && drush runserver http://127.0.0.1:8888 &
 fi
 
